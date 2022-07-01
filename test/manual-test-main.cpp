afcd0fe7128769979186cbb52d82fc0a7e2c112d
Barnabás Bágyi <barnabas.bagyi@shapr3d.com>
2022-07-01 22:58:01 +0200

    update CLI test exe

--- test/manual-test-main.cpp
+++ test/manual-test-main.cpp
@@ -1,25 +1,26 @@
-#include "measure-calculator/measure-calculator.hpp"
 #include "measure-calculator/defaults.hpp"
+#include "measure-calculator/measure-calculator.hpp"
 
 #include <iostream>
 
 using namespace Calc;
 
-int main(int argc, char** argv ) {
-    std::string to_eval= "";
+int main(int argc, char** argv) {
+    std::string to_eval = "";
     for (int i = 1; i < argc; ++i) {
         to_eval.append(argv[i]);
     }
 
-    auto spec = std::get<Spec>(SpecBuilder {
-        .unary_ops = Defaults::kNegateUnaryOp,
-        .binary_ops = Defaults::kArithmeticBinaryOps,
-        .unary_funs = Defaults::kBasicUnaryFuns + Defaults::kExponentialUnaryFuns +
-                    Defaults::kTrigonometricUnaryFuns,
-        .binary_funs = Defaults::kBasicBinaryFuns,
+    auto spec = std::get<Spec>(SpecBuilder{
+        .unaryOps = Defaults::kNegateUnaryOp,
+        .binaryOps = Defaults::kArithmeticBinaryOps,
+        .unaryFuns = SpecUnion(Defaults::kBasicUnaryFuns, Defaults::kExponentialUnaryFuns,
+                               Defaults::kTrigonometricUnaryFuns),
+        .binaryFuns = Defaults::kBasicBinaryFuns,
         .constants = Defaults::kBasicConstants,
         .measures = {Defaults::kLinearMeasure},
-    }.Build());
+    }
+                                   .Build());
 
     auto res = Evaluate(spec, to_eval);
     if (auto d = std::get_if<double>(&res)) {
